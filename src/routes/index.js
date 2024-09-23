70c3e6b32ee7cb4fa9674e90c8b086fe8a671dee
DongTang <eliuinz@gmail.com>
2024-09-23 13:21:09 -0400

    init

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
-import AboutPage from '../pages/AboutPage';
+import AboutPage from '../pages/LoginPage';
 
 const AppRoutes = () => (
   <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
 
-      <Route path="/about" element={<AboutPage />} />
+      <Route path="/login" element={<LoginPage  />} />
     </Routes>
   </Router>
 );
