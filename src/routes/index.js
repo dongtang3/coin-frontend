b5bc3ba6df59a7c009513635537c39ccf5e40670
DongTang <eliuinz@gmail.com>
2024-10-07 18:30:49 -0400

    init

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
-import AboutPage from '../pages/LoginPage';
+import LoginPage from '../pages/LoginPage';
 
 const AppRoutes = () => (
   <Router>
